cc834145b998189d83d602adcba4aad7a1402709
Nick Stracke <nick.stracke@web.de>
2018-06-15 19:24:51 +0200

    add command line interface2

--- NJbTCP.py
+++ NJbTCP.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mode", help="choose the mode", default="c")
-    parser.add_argument("-i", "--ip", help="ip address", default="localhost")
+    parser.add_argument("-i", "--ip", help="ip address", default="0.0.0.0")
     parser.add_argument("-p", "--port", help="port", type=int, default=11000)
     args = parser.parse_args()
 
